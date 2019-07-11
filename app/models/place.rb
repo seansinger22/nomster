19834a51493cd3ecaa0076e80b1779011d92fa94
seansinger22 <seansinger22@gmail.com>
2019-07-11 17:21:39 +0000

    Validations added to all fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   paginates_per 10
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
