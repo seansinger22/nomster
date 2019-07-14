aed6c032aa37fb97097f2b5141829b295d7dd31e
seansinger22 <seansinger22@gmail.com>
2019-07-14 22:34:43 +0000

    Database set up for user comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,13 @@
 class Place < ApplicationRecord
   paginates_per 10
+  
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
+  after_validation :geocode
+  
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, presence: true
   validates :description, presence: true
-  after_validation :geocode
 end
