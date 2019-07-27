5cf052df9825a0cfff6c1b8155ebc8a170cd5346
seansinger22 <seansinger22@gmail.com>
2019-07-27 01:27:33 +0000

    Lesson 41 set up a test email, whic was sent without error but was blocked by gmail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "seansinger22@gmail.com", subject: "A comment has been added to your place")
+    mail(to: "seansingermailer@gmail.com", subject: "A comment has been added to your place")
   end
-  
+
 end
